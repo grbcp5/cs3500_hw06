5d51d19bbcf08781d6f5c898487236dd57162982
grbcp5 <grbcp5@mst.edu>
2017-11-13 00:24:18 -0600

    Print all black row

--- broadwaterg.lisp
+++ broadwaterg.lisp
@@ -11,8 +11,52 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(write-line "Hello, world!")
+(defun allBlackRow(size)
+  "Returns an all black row"
+  (let* ((row ()))
+    (dotimes (i size)
+      (setf row 
+        (cons 'b row)
+      )
+    )
+  row
+  )
+)
 
+(defun printRow(row)
+  "Prints a row of red/black cells"
+  (format t "(~{#\\~a~})~%" row)
+)
+
+(defun placeRedBlockUnit (
+  minRedBlockUnitSize 
+  redBlockUnitSize 
+  rowSize 
+  start 
+  configurations 
+  prevString
+  )
+  "Places all possible red block units possible starting with given configuration"
+  (printRow prevString)
+)
+
+(defun placeRedBlocks (size) 
+  "Returns a list of all the possible red block combinations in a row"
+  
+  (defvar allBlack (allBlackRow size))
+  
+  (placeRedBlockUnit
+    0
+    0
+    0
+    0
+    ()
+    allBlack
+  ) 
+)
+
+(write-line "Hello, world!")
+(placeRedBlocks 7)
 
 ;;; Exit out of gcl
 (quit)
