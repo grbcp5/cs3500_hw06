44855b6421ee38e3e64bc2e3636f718c3564945e
grbcp5 <grbcp5@mst.edu>
2017-11-13 01:08:23 -0600

    Place one red unit

--- broadwaterg.lisp
+++ broadwaterg.lisp
@@ -33,29 +33,44 @@
   redBlockUnitSize 
   rowSize 
   start 
-  configurations 
   prevConfig
   )
   "Places all possible red block units possible starting with given configuration"
-  (setf newConfig (copy-list prevConfig))
-  (setf (nth 1 newConfig) 'r)
-  (printRow prevConfig)
-  (printRow newConfig)
+  (setf config (copy-list prevConfig))
+
+  (if (>= rowSize (+ redBlockUnitSize start))
+    (progn
+      (loop for pos from 0 to (- redBlockUnitSize 1) by 1 do
+        (setf (nth (+ start pos) config) 'r)
+      )
+      (printRow config)
+    )
+  )
+
 )
 
-(defun placeRedBlocks (size) 
+(defun placeRedBlocks (rowSize) 
   "Returns a list of all the possible red block combinations in a row"
   
-  (defvar allBlack (allBlackRow size))
+  (defvar allBlack (allBlackRow rowSize))
+  (defvar minRedBlockUnitSize 3)
+
+  (printRow allBlack)
+
   
-  (placeRedBlockUnit
-    0
-    0
-    0
-    0
-    ()
-    allBlack
-  ) 
+  (defvar unitSize 3)
+   ;(loop for unitSize from minRedBlockUnitSize to rowSize by 1 do
+    (loop for start from 0 to rowSize by 1 do
+      (placeRedBlockUnit 
+        minRedBlockUnitSize
+        unitSize
+        rowSize
+        start
+        allBlack
+      )
+    )
+  ;)
+
 )
 
 (write-line "Hello, world!")
